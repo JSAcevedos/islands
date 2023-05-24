6ef7fc0bd0f69b3d446bf42f3d60a7433751ae9c
Juan Acevedo <juan.acevedo9805@gmail.com>
2023-05-23 19:54:24 -0500

    First methods and classes creation

---
+++ main.py
@@ -0,0 +1,11 @@
+class Node:
+    def _init_(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class Tree:
+    def _init_(self):
+        self.root = None
+
+    
